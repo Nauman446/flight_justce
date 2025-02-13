827f73e5b65384b90b9c8c0b53936e6e2943d260
Nauman Rehmat <naumanrehmat.1@gmail.com>
2025-02-13 23:09:58 +0500

    Update flight_justice.py

--- flight_justice.py
+++ flight_justice.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "AIzaSyBNgXnIkx1-az2w-qGgRdN4AmQipmhoLpY"
+API_KEY = "AIzaSyAQsMsIMTuFnnT1D72pu19vMxc6GzJ0xtM"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
